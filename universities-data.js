271709b3a69f20a093c6abcd29974012f8881f70
lounwb <lounwb@gmail.com>
2025-09-09 22:16:29 +0800

    refactor: 新增vercel配置

--- universities-data.js
+++ universities-data.js
@@ -13,18 +13,54 @@ const universitiesData = {
     },
     "电子科技大学（清水河校区）": {
         "address": "成都市高新区（西区）西源大道2006号",
-        "classesPerDay": null,
-        "classTimes": []
+        "classesPerDay": 11,
+        "classTimes": [
+            { start: "08:30", end: "09:15" },
+            { start: "09:20", end: "10:05" },
+            { start: "10:20", end: "11:05" },
+            { start: "11:10", end: "11:55" },
+            { start: "14:30", end: "15:15" },
+            { start: "15:20", end: "16:05" },
+            { start: "16:20", end: "17:05" },
+            { start: "17:10", end: "17:55" },
+            { start: "19:30", end: "20:15" },
+            { start: "20:20", end: "21:05" },
+            { start: "21:10", end: "21:55" }
+        ]
     },
     "电子科技大学（沙河校区）": {
         "address": "成都市成华区建设北路二段4号",
-        "classesPerDay": null,
-        "classTimes": []
+        "classesPerDay": 11,
+        "classTimes": [
+            { start: "08:30", end: "09:15" },
+            { start: "09:20", end: "10:05" },
+            { start: "10:20", end: "11:05" },
+            { start: "11:10", end: "11:55" },
+            { start: "14:30", end: "15:15" },
+            { start: "15:20", end: "16:05" },
+            { start: "16:20", end: "17:05" },
+            { start: "17:10", end: "17:55" },
+            { start: "19:30", end: "20:15" },
+            { start: "20:20", end: "21:05" },
+            { start: "21:10", end: "21:55" }
+        ]
     },
     "电子科技大学（九里堤校区）": {
         "address": "成都市金牛区九里堤西路8号",
-        "classesPerDay": null,
-        "classTimes": []
+        "classesPerDay": 11,
+        "classTimes": [
+            { start: "08:30", end: "09:15" },
+            { start: "09:20", end: "10:05" },
+            { start: "10:20", end: "11:05" },
+            { start: "11:10", end: "11:55" },
+            { start: "14:30", end: "15:15" },
+            { start: "15:20", end: "16:05" },
+            { start: "16:20", end: "17:05" },
+            { start: "17:10", end: "17:55" },
+            { start: "19:30", end: "20:15" },
+            { start: "20:20", end: "21:05" },
+            { start: "21:10", end: "21:55" }
+        ]
     },
     "四川大学（望江校区）": {
         "address": "成都市武侯区一环路南一段24号",
@@ -233,48 +269,48 @@ const universitiesData = {
     },
     "中国石油大学（华东）（古镇口校区）": {
         "address": "山东省青岛市黄岛区海军路6号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "合肥工业大学（宣城校区）": {
         "address": "安徽省宣城市宣州区薰化路301号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "武汉理工大学（余家头校区）": {
         "address": "武汉市武昌区和平大道1178号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "中山大学（深圳校区）": {
         "address": "深圳市光明区公常路66号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "复旦大学（张江校区）": {
         "address": "上海市浦东新区张衡路825号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "上海交通大学（闵行校区）": {
         "address": "上海市闵行区东川路800号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "山东大学（威海校区）": {
         "address": "山东省威海市文化西路180号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "山东大学（青岛校区）": {
         "address": "山东省青岛市即墨区滨海路72号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "北京交通大学（威海校区）": {
         "address": "山东省威海市南海新区现代路69号",
-            "classesPerDay": null,
-                "classTimes": []
+        "classesPerDay": null,
+        "classTimes": []
     },
     "南京理工大学（江阴校区）": {
         "address": "江苏省无锡市江阴市福星路8号",
